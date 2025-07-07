06f6f3f9114f3ced45850d3528d51fe2c695db3f
aessary <raressilviulazar@gmail.com>
2025-07-07 11:28:15 +0100

    Routing Example

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 
 export default function Index() {
@@ -7,6 +8,8 @@ export default function Index() {
       className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-primary font-bold"> Welcome M! </Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avenger">Avenger Movie</Link>
     </View>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+
+export default function Onboarding() {
+    return (
+        <View>
+            <Text>Onboarding</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({})
